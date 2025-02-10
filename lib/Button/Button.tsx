6e57fe8c154256dbda20601518d023efaae6ad1a
goncang1512 <samuderanst@gmail.com>
2025-02-10 13:50:20 +0700

    feat: add component button

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,13 +1,54 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import { cn } from "../utils";
+import { cva, VariantProps } from "class-variance-authority";
 
-interface ButtonProps {
-  children: ReactNode;
+interface ButtonProps
+  extends VariantProps<typeof buttonVariants>,
+    ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: ReactNode;
   className?: string;
 }
+
+const buttonVariants = cva("cursor-pointer duration-150", {
+  variants: {
+    variant: {
+      primary: "bg-blue-600 hover:bg-blue-500 text-white rounded-md",
+      secondry: "bg-gray-600 hover:bg-gray-500 text-white rounded-md",
+      success: "bg-green-600 hover:bg-green-500 text-white rounded-md",
+      danger: "bg-red-600 hover:bg-red-500 text-white rounded-md",
+      warning: "bg-yellow-500 hover:bg-yellow-400 text-black rounded-md",
+      info: "bg-cyan-500 hover:bg-cyan-400 text-white rounded-md",
+      outline:
+        "border border-blue-600 text-blue-600 hover:bg-blue-100 rounded-md",
+      gost: "text-gray-700 hover:bg-gray-200 rounded-md",
+      link: "text-blue-600 hover:underline rounded-md",
+      dark: "bg-slate-800 hover:bg-slate-700 text-white rounded-md",
+    },
+    size: {
+      small: "px-2 py-1 text-sm",
+      normal: "px-4 py-2 text-base",
+      large: "px-6 py-3 text-lg",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "normal",
+  },
+});
 
 export const Button: React.FC<ButtonProps> = ({
   children,
   className,
+  variant,
+  size,
+  ...props
 }): ReactNode => {
-  return <button className={`${className}`}>{children}</button>;
+  return (
+    <button
+      className={cn(buttonVariants({ className, variant, size }))}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Button } from "mogo-ui";
 function App() {
   return (
     <>
-      <div className="h-screen bg-blue-200">
-        <Button className="bg-blue-500">sfasdfasdfasdf</Button>
+      <div className="p-5">
+        <Button variant={"dark"} onClick={() => console.log("Hello world")}>
+          on Click
+        </Button>
       </div>
     </>
   );
