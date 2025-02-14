f4c97942daea1629d6f201cc32781916d6b62ccb
goncang1512 <samuderanst@gmail.com>
2025-02-14 22:13:20 +0700

    fix: clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,4 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [darkMode, setDarkMode] = useState(
-    localStorage.getItem("theme") === "dark"
-  );
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [darkMode]);
-
   return <div>hello world</div>;
 }
 
