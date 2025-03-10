e0d95e735823543543d048071f2728abde1d4149
goncang1512 <samuderanst@gmail.com>
2025-03-10 09:37:15 +0700

    fix: change outline in input style v0.6.6

--- lib/Input/Input.tsx
+++ lib/Input/Input.tsx
@@ -12,7 +12,7 @@ const inputVariants = cva("", {
   variants: {
     variant: {
       default:
-        "border-2 dark:border-slate-600 border-slate-300 rounded-md outline-none focus:ring-2 focus:border-none text-black dark:text-slate-200",
+        "border-2 dark:border-slate-600 border-slate-300 rounded-md focus:outline text-black dark:text-slate-200",
       underline:
         "border-b-2 outline-none dark:border-slate-600 border-slate-300 text-black dark:text-slate-200",
     },
@@ -22,12 +22,12 @@ const inputVariants = cva("", {
       large: "px-6 py-3 text-lg",
     },
     theme: {
-      primary: "focus:ring-blue-500 focus:border-blue-500",
-      secondary: "focus:ring-gray-600 focus:border-gray-600",
-      danger: "focus:ring-red-600 focus:border-red-600",
-      warning: "focus:ring-yellow-500 focus:border-yellow-500",
-      success: "focus:ring-green-600 focus:border-green-600",
-      info: "focus:ring-cyan-500 focus:border-cyan-500",
+      primary: "focus:outline-blue-500 focus:border-blue-500",
+      secondary: "focus:outline-gray-600 focus:border-gray-600",
+      danger: "focus:outline-red-600 focus:border-red-600",
+      warning: "focus:outline-yellow-500 focus:border-yellow-500",
+      success: "focus:outline-green-600 focus:border-green-600",
+      info: "focus:outline-cyan-500 focus:border-cyan-500",
     },
   },
   defaultVariants: {
--- lib/Textarea/Textarea.tsx
+++ lib/Textarea/Textarea.tsx
@@ -10,7 +10,7 @@ const texareaVariant = cva("", {
   variants: {
     variant: {
       default:
-        "border-2 dark:border-slate-600 border-slate-300 rounded-md outline-none focus:ring-2 focus:border-none text-black dark:text-slate-200",
+        "border-2 dark:border-slate-600 border-slate-300 rounded-md focus:outline text-black dark:text-slate-200",
       underline:
         "border-b-2 outline-none dark:border-slate-600 border-slate-300 text-black dark:text-slate-200",
     },
@@ -20,12 +20,12 @@ const texareaVariant = cva("", {
       large: "px-6 py-3 text-lg",
     },
     theme: {
-      primary: "focus:ring-blue-500 focus:border-blue-500",
-      secondary: "focus:ring-gray-600 focus:border-gray-600",
-      danger: "focus:ring-red-600 focus:border-red-600",
-      warning: "focus:ring-yellow-500 focus:border-yellow-500",
-      success: "focus:ring-green-600 focus:border-green-600",
-      info: "focus:ring-cyan-500 focus:border-cyan-500",
+      primary: "focus:outline-blue-500 focus:border-blue-500",
+      secondary: "focus:outline-gray-600 focus:border-gray-600",
+      danger: "focus:outline-red-600 focus:border-red-600",
+      warning: "focus:outline-yellow-500 focus:border-yellow-500",
+      success: "focus:outline-green-600 focus:border-green-600",
+      info: "focus:outline-cyan-500 focus:border-cyan-500",
     },
   },
   defaultVariants: {
